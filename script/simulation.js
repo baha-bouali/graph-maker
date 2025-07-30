393a3f31603d5c84bc31ffe1eb8bf98bc86d44d0
baha bouali <baha.bouali2020@gmail.com>
2025-07-30 15:26:28 +0100

    smoother simulation

--- script/simulation.js
+++ script/simulation.js
@@ -8,9 +8,24 @@ export function createSimulation(svg) {
     const links = [];
 
     const simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.id).distance(100))
-    .force("charge", d3.forceManyBody().strength(-300))
-    .force("center", d3.forceCenter(width / 2, height/2));
+    .force("link", d3.forceLink(links)
+        .id(d => d.id)
+        .distance(100)
+        .strength(0.8)
+    )
+    .force("charge", d3.forceManyBody()
+        .strength(-500) // stronger repulsion
+        .distanceMin(50) // min distance before repulsion
+        .distanceMax(400) // Maximum distance for repulsion
+    )
+    // .force("center", d3.forceCenter(width / 2, height/2)); // we don't them to be always centered
+    .force("collision", d3.forceCollide()
+        .radius(25) // Node collision radius
+        .strength(0.7)
+    )
+    .alphaDecay(0.01) // slower cooling for simulation
+    .velocityDecay(0.4); // More fluid Movement
+
 
     return { nodes, links, simulation };
 }
